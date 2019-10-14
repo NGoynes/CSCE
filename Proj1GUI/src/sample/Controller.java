8481205ce379d8a54c0829e2dd3ce4d9ef60aaef
ngoynes <nate.goynes@gmail.com>
2019-10-14 00:35:49 -0500

    315GUI

---
+++ Proj1GUI/src/sample/Controller.java
@@ -0,0 +1,51 @@
+package sample;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.image.ImageView;
+import org.jetbrains.annotations.NotNull;
+
+
+public class Controller implements Initializable {
+    @FXML
+    private ImageView btnTwo, btnThree, btnFour, btnClose;
+
+    @FXML
+    private AnchorPane h_queryTwo, h_queryThree, h_queryFour;
+
+    @FXML
+    private void handleButtonAction(@NotNull MouseEvent event) {
+        if(event.getTarget() == btnTwo) {
+            h_queryTwo.setVisible(true);
+            h_queryThree.setVisible(false);
+            h_queryFour.setVisible(false);
+        }
+        else if (event.getTarget() == btnThree) {
+            h_queryThree.setVisible(true);
+            h_queryTwo.setVisible(false);
+            h_queryFour.setVisible(false);
+        }
+        else if (event.getTarget() == btnFour) {
+            h_queryFour.setVisible(true);
+            h_queryThree.setVisible(false);
+            h_queryTwo.setVisible(false);
+        }
+        else if (event.getTarget() == btnClose) {
+            h_queryFour.setVisible(false);
+            h_queryThree.setVisible(false);
+            h_queryTwo.setVisible(false);
+        }
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+
+    }
+}
+
